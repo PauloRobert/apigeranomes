6b34101e741afb29f56016aa47a48b8960044779
Paulo Roberto <evans_sp@hotmail.com>
2025-03-18 19:26:59 -0300

    Create nomes_data.py

---
+++ app/data/nomes_data.py
@@ -0,0 +1,3 @@
+nomes_masculinos = ["Carlos", "João", "Pedro", "Fernando", "Luiz", "Mateus", "Roberto", "Ricardo"]
+nomes_femininos = ["Ana", "Maria", "Juliana", "Camila", "Beatriz", "Fernanda", "Patrícia", "Renata"]
+sobrenomes = ["Silva", "Santos", "Oliveira", "Souza", "Lima", "Carvalho", "Gomes", "Ribeiro"]
