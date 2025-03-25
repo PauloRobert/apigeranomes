f2689ecbb8936b3f97a4c6b156aef2d97e4ee1ae
Paulo Roberto <evans_sp@hotmail.com>
2025-03-25 20:51:14 -0300

    Criando uma rota para o Swagger

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
 from app.routes import nomes
 
 app = FastAPI(title="API Geradora de Nomes",
@@ -8,6 +10,12 @@ app = FastAPI(title="API Geradora de Nomes",
 
 app.include_router(nomes.router)
 
+# Criando uma rota raiz para redirecionar para o Swagger UI
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")  # Redireciona automaticamente para a documentação do Swagger
+
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
